de0ecb9ae8d31746b67d9d7d0fa070f61fd93cf5
BEATRIZ MARTINS OLIVEIRA <44416789831@info.local>
2025-09-26 19:22:47 -0300

    Notificações realizadas na API e no stramlit

---
+++ db_umidade_leitura.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: db_umidade
+-- ------------------------------------------------------
+-- Server version	8.0.38
+-- CREATE DATABASE db_umidade;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `leitura`
+--
+USE db_umidade;
+DROP TABLE IF EXISTS `leitura`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `leitura` (
+  `id_leitura` int NOT NULL AUTO_INCREMENT,
+  `umidade` decimal(5,2) NOT NULL,
+  `altitude` decimal(5,2) NOT NULL,
+  `temperatura` decimal(5,2) NOT NULL,
+  `pressao` decimal(10,2) NOT NULL,
+  `co2` int NOT NULL,
+  `tempo_registro` datetime NOT NULL,
+  PRIMARY KEY (`id_leitura`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `leitura`
+--
+
+LOCK TABLES `leitura` WRITE;
+/*!40000 ALTER TABLE `leitura` DISABLE KEYS */;
+/*!40000 ALTER TABLE `leitura` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-09-25 19:51:56
+
+SELECT * FROM leitura;
